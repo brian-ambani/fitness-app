c39fcf1068357049f815dfcf811310f59b93ec52
brian-ambani <brainambani1@gmail.com>
2024-01-03 13:17:10 +0300

    updated the SearchExercise component

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,5 +1,48 @@
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relatve" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700px", border: "none", borderRadius: "4px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#FFFFFF",
+            borderRadius: "40px",
+          }}
+          height="70px"
+          value=""
+          onChange={() => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absoulute",
+            right: "0",
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
