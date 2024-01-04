778d088027e4a89038169754da204df3938e9fdb
brian-ambani <brainambani1@gmail.com>
2024-01-04 04:17:59 +0300

    updated the states in the home page file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box } from "@mui/material";
 
 import HeroBanner from "../components/HeroBanner";
@@ -5,11 +6,22 @@ import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
