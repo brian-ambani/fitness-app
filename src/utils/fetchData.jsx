ec8cb96bf0e00c4d691a99a19fd600e8084cbee6
brian-ambani <brainambani1@gmail.com>
2024-01-04 04:18:34 +0300

    updated the function to fetch data from the API

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,1 +1,13 @@
-export const fetchData = async () => {};
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
